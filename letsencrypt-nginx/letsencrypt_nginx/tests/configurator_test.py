ffc45e7f0c28d8ff86fbe16174081b242215dfb7
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-01-04 15:02:15 -0500

    nginx plugin requires fullchain_path

--- letsencrypt-nginx/letsencrypt_nginx/tests/configurator_test.py
+++ letsencrypt-nginx/letsencrypt_nginx/tests/configurator_test.py
@@ -136,6 +136,16 @@ class NginxConfiguratorTest(util.NginxTest):
             None,
             None)
 
+    def test_deploy_cert_requires_fullchain_path(self):
+        self.config.version = (1, 3, 1)
+
+        self.assertRaises(errors.PluginError, self.config.deploy_cert,
+            "www.example.com",
+            "example/cert.pem",
+            "example/key.pem",
+            "example/chain.pem",
+            None)
+
     def test_deploy_cert(self):
         server_conf = self.config.parser.abs_path('server.conf')
         nginx_conf = self.config.parser.abs_path('nginx.conf')
